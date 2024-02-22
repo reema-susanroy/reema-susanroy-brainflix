7682efe42fff28bf199d93c0e0a794c2f2d7865f
Reema Roy <reemaroy73@gmail.com>
2024-02-21 16:14:43 -0800

    Implemented Navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import './Header.scss';
+
+import logo from '../../assets/images/logo/BrainFlix-logo.svg'
+import avatar from '../../assets/images/Mohan-muruge.jpg'
+
+function Header() {
+    return (
+        <header>
+            <nav className='navbar'>
+                <div className='navbar__logo navbar__left'>
+                    <img className='navbar__logo--item' src={logo} alt='brainflix-logo' />
+                </div>
+                <div className='navbar__side-container navbar__right'>
+                    <div className='navbar__search navbar__logo'>
+                        <form className='navbar__search-form'>
+                            <label htmlFor='search'></label>
+                            <input id="search" name='input' placeholder='Search' />
+                        </form>
+                        <div className='navbar__image-mobile '>
+                            <img src={avatar} alt='avatar' />
+                        </div>
+                    </div>
+                    <div className='navbar__button'>
+                        <button className='navbar__button--upload'>Upload</button>
+                    </div>
+                    <div className='navbar__image-tablet'>
+                        <img src={avatar} alt='avatar' />
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
