5b1583078204a0595952d492ca80c661d9e8d663
Reema Roy <reemaroy73@gmail.com>
2024-02-23 12:12:24 -0800

    Update date format from timestamp to mm-dd-yyyy format for video description section.

--- src/components/CommentDisplay/CommentDisplay.js
+++ src/components/CommentDisplay/CommentDisplay.js
@@ -3,26 +3,26 @@ import './CommentDisplay.scss';
 function CommentDisplay({ comment }) {
 
     let time = comment.timestamp;
-    function timeFormat(timeVal){
-        const currentTime = new Date().getTime();
-        const commentDate = comment.timestamp;
-        const calcTime = currentTime - commentDate;
-        const seconds = Math.floor(calcTime / 1000);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
-        let time;
-        if (days > 0) {
-            time = days + ' day' + (days > 1 ? 's' : '') + ' ago';
-        } else if (hours > 0) {
-            time = hours + ' hour' + (hours > 1 ? 's' : '') + ' ago';
-        } else if (minutes > 0) {
-            time = minutes + ' minute' + (minutes > 1 ? 's' : '') + ' ago';
-        } else {
-            time = '< 1 min ago';
-        }
-        return time;
-    }
+    // function timeFormat(timeVal){
+    //     const currentTime = new Date().getTime();
+    //     const commentDate = comment.timestamp;
+    //     const calcTime = currentTime - commentDate;
+    //     const seconds = Math.floor(calcTime / 1000);
+    //     const minutes = Math.floor(seconds / 60);
+    //     const hours = Math.floor(minutes / 60);
+    //     const days = Math.floor(hours / 24);
+    //     let time;
+    //     if (days > 0) {
+    //         time = days + ' day' + (days > 1 ? 's' : '') + ' ago';
+    //     } else if (hours > 0) {
+    //         time = hours + ' hour' + (hours > 1 ? 's' : '') + ' ago';
+    //     } else if (minutes > 0) {
+    //         time = minutes + ' minute' + (minutes > 1 ? 's' : '') + ' ago';
+    //     } else {
+    //         time = '< 1 min ago';
+    //     }
+    //     return time;
+    // }
 
     timeFormat(time);
     return (
@@ -49,3 +49,24 @@ function CommentDisplay({ comment }) {
 
 
 export default CommentDisplay;
+
+export function timeFormat(timeVal){
+    const currentTime = new Date().getTime();
+    const commentDate = timeVal;
+    const calcTime = currentTime - commentDate;
+    const seconds = Math.floor(calcTime / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    let time;
+    if (days > 0) {
+        time = days + ' day' + (days > 1 ? 's' : '') + ' ago';
+    } else if (hours > 0) {
+        time = hours + ' hour' + (hours > 1 ? 's' : '') + ' ago';
+    } else if (minutes > 0) {
+        time = minutes + ' minute' + (minutes > 1 ? 's' : '') + ' ago';
+    } else {
+        time = '< 1 min ago';
+    }
+    return time;
+}
--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -1,5 +1,6 @@
 // import Video from '../Video/Video';
 import './VideoDetails.scss';
+// import {timeFormat} from '../CommentDisplay/CommentDisplay';
 import viewsIcon from '../../assets/images/icons/views.svg'
 import likesIcon from '../../assets/images/icons/likes.svg'
 
@@ -8,13 +9,26 @@ function VideoDetails({ mainVideo }) {
     console.log("Inside videosDeayils: " + { mainVideo });
     const {channel,timestamp,views,likes,description} = mainVideo;
 
+    //calling timeFormat function
+    const date = new Date(timestamp)
+    const today = new Date();
+
+    // const day = dayArray[date.getDay()];
+    // const dateValue = ((String(date.getDate()).length) === 2 ? '' : '0') + date.getDate();
+    const dateValue = date.getDate();
+    const month = (date.getMonth()+1);
+    const year =date.getFullYear();
+
+
+    console.log(date)
+    console.log(today);
     return (
         <div>
             <h1 className='video__title'>{mainVideo.title}</h1>
             <div className='video__container'>
                 <div className='video__data'>
                     <p className='video__data--channel'>By {channel}</p>
-                    <p className='video__data--time'>{timestamp}</p>
+                    <p className='video__data--time'>{`${month}/${dateValue}/${year}`}</p>
                 </div>
                 <div className='video__social'>
                     <div className='video__social--icons'>
