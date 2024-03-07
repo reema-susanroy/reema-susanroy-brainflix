4f04520e44d67aee4e72b0d7725be7f441adf5e1
Reema Roy <reemaroy73@gmail.com>
2024-03-06 18:48:49 -0800

    Implement Put endpoint for likes

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -1,13 +1,20 @@
+import axios from 'axios';
 import './VideoDetails.scss';
 import viewsIcon from '../../assets/images/icons/views.svg'
 import likesIcon from '../../assets/images/icons/likes.svg'
 import {TimeFormat} from '../../utils/TimeFormat';
+import { useState } from 'react';
 
 // Component to display the video details based on the video object passed as a prop from the Video component
 // Props are destructured and accessed in this component 
 function VideoDetails({ mainVideo }) {
-    const { channel, timestamp, views, likes, description } = mainVideo;
+    const { id, channel, timestamp, views, likes, description } = mainVideo;
+    const [likeCount, setLikeCount] =useState(likes)
 
+    const handleLike = async (id) =>{
+        const response = await axios.put(`http://localhost:8080/videos/${id}/likes`)
+        setLikeCount(response.data);
+    }
     return (
         <>
             <h1 className='video__title'>{mainVideo.title}</h1>
@@ -22,8 +29,8 @@ function VideoDetails({ mainVideo }) {
                         <p>{views}</p>
                     </div>
                     <div className='video__social--icons'>
-                        <img src={likesIcon} alt='like-icon' />
-                        <p>{likes}</p>
+                        <img onClick={() => handleLike(id)} src={likesIcon} alt='like-icon' />
+                        <p>{likeCount}</p>
                     </div>
                 </div>
             </div>
